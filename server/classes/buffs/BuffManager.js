85d3c6d7ed86fa2f361f5f7123c07781c8f86e28
uyas2207 <uyas2207@mail.ru>
2026-04-19 15:29:29 +0300

    added comments and private methods in BuffManager class

--- server/classes/buffs/BuffManager.js
+++ server/classes/buffs/BuffManager.js
@@ -4,10 +4,8 @@ import * as chat from 'alt:chat';
 export class BuffManager {
     constructor(buffStorage, buffInfoList, baseObjectType, pedStorage) {
         this.buffStorage = buffStorage;
-        this.buffInfoList = buffInfoList;
-        this.baseObjectType = baseObjectType;
-
-        this.pedStorage = pedStorage;   //для тестов потом надо убрать
+        this.buffInfoList = buffInfoList;       //данные из конфига о существующих бафах и их характеристиках
+        this.baseObjectType = baseObjectType;   //данные из конфига о BaseObjectType.id соответвующие Player, Vehicle, Ped
 
         this.#registerEventListeners();
     }
@@ -36,69 +34,30 @@ export class BuffManager {
         });
     }
 
-    validate_TargetType1(targetType){
-        //const targetType = parseInt(targetType);
-        if(Object.values(this.baseObjectType).includes(targetType)){
-            return targetType;
-        }
-        else{
-            return undefined;
-        }
-        /*
-        const maxObjectType = Object.keys(this.baseObjectType).length-1;
-        if (isNaN(targetType) || targetType < 0 || targetType > maxObjectType){
-            alt.log('Неправильный аругмент, аргументом может быть только целое число от 0 до', maxObjectType);
-            return undefined;
-        }
-        alt.log('Корректный targetType =', targetType);
-        return targetType;
-        */
+    //выводит сообщение со списком всех бафов из конфига
+    printAllExistingMessage(){
+        alt.log('Существующие бафы:');
+        Object.keys(this.buffInfoList).forEach(buff => {
+            alt.log(`${buff}`);
+        });
     }
-
-    validate_BuffName(buffName){
-       if (!(buffName in this.buffInfoList)){
-            this.printAllExistingMessage(); //выводит сообщение со списком всех бафов из конфига
-            throw new Error(` Бафа ${buffName} не существует`);
-       }
-    }
-
-    validate_TargetType(targetType){
-        if (typeof targetType === "string"){
-            targetType = this.baseObjectType[targetType];
-        }
-
-        if(!(Object.values(this.baseObjectType).includes(targetType))){
-            throw new Error(` targetType ${targetType} не существует в baseObjectType`);
-        }
-        return targetType;
-    }
-
-    validate_targetId(targetType, targetId){
-        const entity = alt.BaseObject.getByID(targetType, targetId);
-
-        if(entity === null){
-            throw new Error('Ошибка в методе validate_targetId, запрашиваемый entity не существует');
-        }
-
-        return entity;
-    }
-    //alt.BaseObject.getByID(this.baseObjectType[targetType], targetId)
+    //основной метод класса, проверяет коретность данных и если все правильно записывает баф в класс buffStorage
     add_buff(player, arg) {
-        const buffName = String(arg[0]);
-        let targetType = arg[1];
-        const targetId = arg[2];
+        const buffName = String(arg[0]);    //medicalHelp, drunk, armor_regen, fear, invisible
+        let targetType = arg[1];            // Player, Vehicle, Ped или сразу BaseObjectType.id (0, 1, 2)
+        const targetId = arg[2];            // id entety которому будет назначен баф (например Player с id=2)
 
-        let stacksAmmount = 1;    //изначальное значение стаков бафа
-        const now = Date.now();
-
-        this.validate_BuffName(buffName);
-        targetType = this.validate_TargetType(targetType);
-        const entity = this.validate_targetId(targetType, targetId);
-
-        if ((this.isStackableBuff(buffName) === true) && (this.buffStorage.hasActiveBuff(entity, buffName))){
-            stacksAmmount = this.getBuffStacksAmmount(entity, buffName) + 1;
+        let stacksAmmount = 1;      //изначальное значение стаков бафа
+        const now = Date.now();     //время в которое назначен баф
+        //проверка коректности полученных данных, если данные некорректные код дальше не идет
+        this.#validate_BuffName(buffName);   //проверяет существует ли баф с таким названием в конфиге, если нет останваливает выполнение кода
+        targetType = this.#validate_TargetType(targetType);  //переводит Player, Vehicle, Ped в BaseObjectType.id (при необходимости) и проверяет сущетсует ли такой BaseObjectType.id в конифиге, если нет останваливает выполнение кода
+        const entity = this.#validate_TargetId(targetType, targetId);    //проверяет существует ли BaseObjectType с таким id на сервере, если да возвращает этот entity, если нет останваливает выполнение кода
+        //проверяет можно ли стакать баф и есть ли такой баф на таком энтити, если есть проверяет что количество стаков не больше максимума
+        if ((this.#isStackableBuff(buffName) === true) && (this.buffStorage.hasActiveBuff(entity, buffName))){
+            stacksAmmount = this.#handleBuffStacking(entity, buffName);  //если количество стаков меньше максимума вернет стак+1, если количество стаков больше или равно максимум вернет знаечние равное текущему кол-ву стаков
         }
-        
+        //данные которые будут записаны соответвующему entity в соответсвующий buffName
         const instance = {
             buffName: this.buffInfoList[buffName].name,
             stacks: stacksAmmount,
@@ -107,110 +66,54 @@ export class BuffManager {
             tickInterval: this.buffInfoList[buffName].tickInterval,
         };
 
-        this.buffStorage.addBuffToMap(entity, buffName, instance);
+        this.buffStorage.addBuffToMap(entity, buffName, instance);  //записывает баф в класс buffStorage
     }
-
-    add_buff1(player, arg) {
-        const buffName = String(arg[0]);
-        let targetType = arg[1];
-        const targetId = arg[2];
-
-        let stacksAmmount = 1;    //изначальное значение стаков бафа
-        const now = Date.now();
-/*
+    //проверяет существует ли баф с таким названием в конфиге, если нет останваливает выполнение кода
+    #validate_BuffName(buffName){
+       if (!(buffName in this.buffInfoList)){
+            this.printAllExistingMessage(); //выводит сообщение со списком всех бафов из конфига
+            throw new Error(`Бафа ${buffName} не существует`);
+       }
+    }
+    //переводит Player, Vehicle, Ped в BaseObjectType.id (при необходимости) и проверяет сущетсует ли такой BaseObjectType.id в конифиге, если нет останваливает выполнение кода
+    #validate_TargetType(targetType){
+        //если targetType записан как название из baseObjectType переводит его в числовое знаенчие по данным из конфига
         if (typeof targetType === "string"){
             targetType = this.baseObjectType[targetType];
         }
-        else{
-            targetType = this.validateTargettype(targetType);
+        //проверяет существует ли такой тип entity в конфиге если нет выдает ошибку и код дальше не идет 
+        if(!(Object.values(this.baseObjectType).includes(targetType))){
+            throw new Error(` targetType ${targetType} не существует в baseObjectType`);
         }
-*/
-        //this.validateTargettype(targetType);
+        //если такой entity существует то возвращает его уже в числовом формате (если он был текстовым)
+        return targetType;
+    }
+    //проверяет существует ли BaseObjectType с таким id на сервере, если да возвращает этот entity, если нет останваливает выполнение кода
+    #validate_TargetId(targetType, targetId){
+        const entity = alt.BaseObject.getByID(targetType, targetId);
 
-        if (this.isValidBuff(buffName) !== true) {
-            alt.log(` Бафа ${buffName} не существует`);
-            this.printAllExistingMessage(); //выводит сообщение со списком всех бафов из конфига
-            return;
+        if(entity === null){
+            throw new Error('Ошибка в методе #validate_TargetId, запрашиваемый entity не существует');
         }
 
-        const buffParameters = this.buffInfoList[buffName];
-
-        if (!buffParameters.allowedEntities.includes(targetType)){
-            alt.logError(`Передан неправильный targetType = ${targetType} для бафа ${buffName}`);
-            return;
-        }
+        return entity;
+    }
+    //проверяет кол-во стаков, если колчиество не максимальное вернет стак+1, если максимальное вернет то же самое значение которое сейчас стоит у бафа
+    #handleBuffStacking(entity, buffName){
+        let stacksAmmount = this.#getBuffStacksAmmount(entity, buffName);    //получает текуще кол-во стаков этого бафа на entity
         
-        const entity = alt.BaseObject.getByID(this.baseObjectType[targetType], targetId)
-        
-        if ((this.isStackableBuff(buffName) === true) && (this.buffStorage.hasActiveBuff(entity, buffName))){
-            stacksAmmount = this.getBuffStacksAmmount(entity, buffName) + 1;
+        if (stacksAmmount < this.buffInfoList[buffName].maxStacks){
+            stacksAmmount++;
         }
 
-        const instance = {
-            buffName: buffParameters.name,
-            stacks: stacksAmmount,
-            appliedAt: now,
-            expiresAt: now + buffParameters.buffDuration,
-            tickInterval: buffParameters.tickInterval,
-        };
-
-        this.buffStorage.addBuffToMap(entity, buffName, instance);
-
-        /*
-        switch (targetType) {
-            case 'Ped':
-                this.add_PedBuff(buffName, targetId, stacksAmmount, buffParameters, now);
-                break;
-            default:
-                alt.logError('Передан несуществующий targetType =', targetType);
-        }
-        */
+        return stacksAmmount
     }
-
-    changeTargetType(targetType){
-        alt.log('this.baseObjectType[targetType]', this.baseObjectType[targetType]);
-        return this.baseObjectType[targetType];
+    //получает текуще кол-во стаков бафа buffName на entity
+    #getBuffStacksAmmount(entity, buffName){
+        return this.buffStorage.getEntityBuff(entity, buffName).stacks;
     }
-
-    /*
-    add_PedBuff(buffName, targetId, stacksAmmount, buffParameters, now){
-        const ped = this.pedStorage.getPed(targetId);
-        //const ped =  alt.Ped.getByID(targetId);
-        //если баф стакаемый и уже существует то увеличвает колличество стаков на 1
-        if ((this.isStackableBuff(buffName) === true) && (this.buffStorage.hasActiveBuff(ped, buffName))){
-            stacksAmmount = this.getBuffStacksAmmount(ped, buffName) + 1;
-        }
-
-        const instance = {
-            buffName: buffParameters.name,
-            stacks: stacksAmmount,
-            appliedAt: now,
-            expiresAt: now + buffParameters.buffDuration,
-            tickInterval: buffParameters.tickInterval,
-        };
-
-        this.buffStorage.addBuffToMap(ped, buffName, instance);
-    
-    }
-*/
-    //выводит сообщение со списком всех бафов из конфига
-    printAllExistingMessage(){
-        alt.log('Существующие бафы:');
-        Object.keys(this.buffInfoList).forEach(buff => {
-            alt.log(`${buff}`);
-        });
-    }
-
-    getBuffStacksAmmount(entity, buffName){
-        const currentBuff = this.buffStorage.getEntityBuff(entity, buffName);
-        return currentBuff.stacks;
-    }
-
-    isStackableBuff(buffName){
+    //проверяет можно ли стакать баф с таким buffName
+    #isStackableBuff(buffName){
         return this.buffInfoList[buffName].stackable;
-    }
-
-    isValidBuff(buffName) {
-        return buffName in this.buffInfoList;
     }
 }
